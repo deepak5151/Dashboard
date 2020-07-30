f1f66fb65d7c2a1994a101d7678c3d0648379274
Deepak Kapoor <nk93555@gmail.com>
2020-07-30 18:40:35 +0530

    Updates

---
+++ precache-manifest.1379e509a83204a0b29958de0753bd35.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6d1ee67da24b4c2bb97c654b51d211",
+    "url": "/Dashboard/index.html"
+  },
+  {
+    "revision": "42ca8102adfc5c851e7e",
+    "url": "/Dashboard/static/css/main.fa6db407.chunk.css"
+  },
+  {
+    "revision": "42654ce6e3b7a53ce6a2",
+    "url": "/Dashboard/static/js/2.f391b33d.chunk.js"
+  },
+  {
+    "revision": "123c36955d3b020d1348e93b9ab88993",
+    "url": "/Dashboard/static/js/2.f391b33d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ca8102adfc5c851e7e",
+    "url": "/Dashboard/static/js/main.362e4efe.chunk.js"
+  },
+  {
+    "revision": "52559d2443dbc8686ae9",
+    "url": "/Dashboard/static/js/runtime-main.28fbf228.js"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/Dashboard/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Dashboard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Dashboard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Dashboard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Dashboard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Dashboard/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/Dashboard/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "82c9299200c18b740fd8466ca95e1b20",
+    "url": "/Dashboard/static/media/logo.82c92992.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/Dashboard/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/Dashboard/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/Dashboard/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/Dashboard/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/Dashboard/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/Dashboard/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/Dashboard/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/Dashboard/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/Dashboard/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
